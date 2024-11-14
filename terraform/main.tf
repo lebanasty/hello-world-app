84bcd51692bf5a4a36b3677868ecb23c27034b64
Christopher Wellington <cawellington1@gmail.com>
2024-11-14 10:26:27 -0500

    apply formatting and comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,61 @@
 provider "aws" {
   region = var.region
 }
+
+# Use the EKS module to simplify cluster setup
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name                             = var.cluster_name
+  cluster_version                          = "1.24" # Replace with latest
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    eks_nodes = {
+      ami_type = "AL2023_x86_64_STANDARD"
+
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "comet_proj"
+  }
+}
+
+# Use the VPC module to simplify VPC setup
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "comet_proj"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,8 @@
 variable "region" {
-    description = "AWS region we're deploying into"
-    default = "us-west-2"
+  description = "AWS region we're deploying into"
+  default     = "us-west-2"
+}
+
+variable "cluster_name" {
+  description = "Name of the k8s cluster"
 }
